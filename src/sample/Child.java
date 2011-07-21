822a95748500d650790e3ef686fdd9e0b2423073
unknown <pshivana@.corporate.thoughtworks.com>
2011-07-21 17:24:53 +0530

    Deleted package

--- src/sample/Child.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-
-public abstract class Child<Context> {
-
-    public abstract void print(Context context);
-}
--- src/sample/ChildOne.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-
-public class ChildOne extends Child<Bean>{
-    @Override
-    public void print(Bean o) {
-       System.out.println( o.message);
-    }
-}
