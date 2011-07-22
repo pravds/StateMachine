7a28bf5ff36c64c88bbb82c89aeac27326daa75e
unknown <pshivana@.corporate.thoughtworks.com>
2011-07-22 17:29:16 +0530

    [Refactored]

--- src/statemachine/EndState.java
+++ src/statemachine/EndState.java
@@ -2,15 +2,9 @@ package statemachine;
 
 public class EndState implements ActionState{
 
-    EndState() {
-        super();
-    }
+    public void action(Object o) {}
 
-    public void action(Object o) {
-    }
-
-    public  void exitAction(Object o) {
-    }
+    public  void exitAction(Object o) {}
 
     public Event nextEvent(Object o) {
         throw new RuntimeException("Unsupported operation") ;
--- src/statemachine/IdleState.java
+++ src/statemachine/IdleState.java
@@ -2,15 +2,9 @@ package statemachine;
 
 public class IdleState implements ActionState{
 
-    IdleState() {
-        super();
-    }
+    public void action(Object o) {}
 
-    public void action(Object o) {
-    }
-
-    public void exitAction(Object o) {
-    }
+    public void exitAction(Object o) {}
 
     public Event nextEvent(Object o) {
         throw new RuntimeException("Unsupported operation") ;
